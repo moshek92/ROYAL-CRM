22cb50d7d98e9da7871fba1db70a5411e79cee0a
=moshe <=moshikkassin@gmail.com>
2022-07-19 21:19:17 +0300

    ROYAL-CRM

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const createError = require('http-errors');
+
+const auth = require('./middleware/auth');
+const headers = require('./middleware/headers');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customersRouter = require('./routes/customers');
+var productsRouter = require('./routes/products');
+var ordersRouter = require('./routes/orders');
+var countriesRouter = require('./routes/countries');
 
 var app = express();
 
@@ -12,9 +20,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'exports')));
 
+app.use(headers);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', auth, usersRouter);
+app.use('/customers', auth, customersRouter);
+app.use('/products', auth, productsRouter);
+app.use('/orders', auth, ordersRouter);
+app.use('/countries', auth, countriesRouter);
+
+// catch 404 err forward error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+})
+
+// custom error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.locals.message = err.message;
+    res.locals.error = err;
+
+    res.status(500).send(err);
+})
 
 module.exports = app;
